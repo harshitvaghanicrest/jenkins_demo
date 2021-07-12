770a8f9312dcaf08a4f92aa2fbfd35478bb4a0c7
harshitvaghanicrest <87296327+harshitvaghanicrest@users.noreply.github.com>
2021-07-12 15:41:53 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-print("Hello Workld!!!")
+import datetime
+version = "1.0.0"
+with open('result.txt', 'w') as file:
+    file.write(f'VERSION: {version}\n')
+    file.write(f"Hello World! @ {datetime.datetime.now()}")
